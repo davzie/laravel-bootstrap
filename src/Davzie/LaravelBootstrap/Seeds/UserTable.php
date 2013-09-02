3475db2a9953d2141737b7c006c5e4b1ef5372d6
David Thorpe <davzie@davzie.com>
2013-09-02 10:53:21 +0100

    Updated At Timestamp Update On Seeding
    Signed-off-by: David Thorpe <davzie@davzie.com>

--- src/Davzie/LaravelBootstrap/Seeds/UserTable.php
+++ src/Davzie/LaravelBootstrap/Seeds/UserTable.php
@@ -17,6 +17,7 @@ class UserTable extends Seeder {
                 'last_name'     => Config::get('laravel-bootstrap::setup.last-name'),
                 'password'      => Hash::make( Config::get('laravel-bootstrap::setup.password') ),
                 'created_at'    => date('Y-m-d H:i:s'),
+                'updated_at'    => date('Y-m-d H:i:s')
             ]
         ];
         DB::table('users')->insert($types);
